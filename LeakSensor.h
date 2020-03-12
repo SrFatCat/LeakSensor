c58333fca0a241d863d40bf577948c784da9a48b
Sr. Fat Cat <matroskin-2004@mail.ru>
2020-03-12 23:35:05 +0300

    Вместо калмана - усреднение

--- LeakSensor.h
+++ LeakSensor.h
@@ -14,26 +14,15 @@ public:
 
 private:
     const uint16_t okAverageDiv = 50;
-    const uint16_t okStDiffDiv = 90;
     const uint16_t sampleSize = 1000;
     
     const uint8_t pin;
     int16_t average;
-    uint16_t standartDif;
+    uint32_t sample = 0;
+    uint16_t sampleCounter = 0;
     
     CLeakSensor::EState state;
     bool isChange = false;
-
-    // переменные для калмана
-    //float varVolt = standartDif;  // среднее отклонение (ищем в begin)
-    float varProcess = 0.001; // скорость реакции на изменение (подбирается вручную)
-    float Pc = 0.0;
-    float G = 0.0;
-    float P = 1.0;
-    float Xp = 0.0;
-    float Zp = 0.0;
-    float Xe = 0.0;
-    // переменные для калмана
 
     virtual uint16_t valueRead() {return analogRead(pin);}
     int16_t filter(float val);
@@ -41,19 +30,26 @@ public:
     CLeakSensor(const uint16_t pin_) :  pin(pin_){}
     virtual void init() {pinMode(pin, INPUT);}
     CLeakSensor::EState begin();
-    void print() {Serial.print("--- Pin A"); Serial.print(pin-54); Serial.print(" avr "); Serial.print(average); Serial.print(" state "); Serial.print(state); Serial.print(" STDIF "); Serial.println(standartDif);}
+    void print() {Serial.print("--- Pin A"); Serial.print(pin-54); Serial.print(" avr "); Serial.print(average); Serial.print(" state "); Serial.println(state);}
     CLeakSensor::EState getState();
     bool getChange(){return isChange;}
 };
 
 CLeakSensor::EState CLeakSensor::getState(){
-    int val = filter(valueRead());
+    sample += valueRead();
+    if (++sampleCounter < sampleSize){
+        isChange = false;
+        return state;
+    }
+    int val = sample / sampleSize;
+    sample = 0;
+    sampleCounter = 0;
     if (state != STATE_START_ERR && val < 200 || val > 650 ){
         isChange = (state != STATE_ERR);
         return state = STATE_ERR;
     }
     else if (state == STATE_START_ERR) return state;
-    else if (abs(average - val) > max(150, standartDif * 3 / 2)){
+    else if (abs(average - val) > 150){
         isChange = (state != STATE_ALARM);
         return state = STATE_ALARM;         
     }
@@ -65,16 +61,6 @@ CLeakSensor::EState CLeakSensor::getState(){
 
 }
 
-int16_t CLeakSensor::filter(float val) {  //функция фильтрации
-    Pc = P + varProcess;
-    G = Pc/(Pc + standartDif);
-    P = (1-G)*Pc;
-    Xp = Xe;
-    Zp = Xp;
-    Xe = G*(val-Zp)+Xp; // "фильтрованное" значение
-    return int(Xe)/10*10;
-}
-
 CLeakSensor::EState CLeakSensor::begin(){
     uint16_t val[sampleSize];
     uint32_t averageTmp = 0;
@@ -83,13 +69,8 @@ CLeakSensor::EState CLeakSensor::begin(){
         averageTmp += val[i];
     }
     average = averageTmp / sampleSize;
-    averageTmp = 0;
-    for (int i=0; i<sampleSize; i++){
-        averageTmp += (val[i] - average)^2;
-    }
-    standartDif = sqrt(averageTmp/sampleSize);
 
-    if (abs(average - 510) > okAverageDiv || standartDif > okStDiffDiv) { 
+    if (abs(average - 510) > okAverageDiv) { 
         Serial.print("!!!!!!!!!!!! ");
         return  state = STATE_START_ERR; 
     }    
--- LeakSensor.ino
+++ LeakSensor.ino
@@ -26,13 +26,12 @@ const char auth[] = "10e3db2a38084530bac9f105d87f7c42";
 
 Adafruit_NeoPixel stripLED(LEAK_CHANELS, PIN_LED_STRIP, NEO_GRB);
 
-enum EColors {BLACK = 0, RED = 0xFF0000, GREEN = 0x00FF00, WHITE = 0xFFFFFF, YELLOW = 0xFFFF00};
+enum EColors {BLACK = 0, RED = 0xFF0000, GREEN = 0x00A000, WHITE = 0xFFFFFF, YELLOW = 0x5F5000};
 
 void beep(uint8_t duration, uint8_t cycles){
 
 }
 
-//CLeakSensor *leakSensor[LEAK_CHANELS];
 CLeakSensor leakSensor[] = {
     CLeakSensor(A13), 
     CLeakSensor(A14), 
@@ -52,6 +51,12 @@ CLeakSensor leakSensor[] = {
     CLeakSensor(A0)
     };
 
+volatile int seconds;
+
+ISR(TIMER1_COMPA_vect){
+    seconds++;
+}
+
 void setup(){
     pinMode(PIN_BUTTON, INPUT_PULLUP);
     pinMode(PIN_BUZZER, OUTPUT);
@@ -70,6 +75,21 @@ void setup(){
         stripLED.show();
         leakSensor[j].print();
     }
+
+    // инициализация Timer1
+    cli();  // отключить глобальные прерывания
+    TCCR1A = 0;   // установить регистры в 0
+    TCCR1B = 0;
+
+    OCR1A = 15624; // установка регистра совпадения
+
+    TCCR1B |= (1 << WGM12);  // включить CTC режим 
+    TCCR1B |= (1 << CS10); // Установить биты на коэффициент деления 1024
+    TCCR1B |= (1 << CS12);
+
+    TIMSK1 |= (1 << OCIE1A);  // включить прерывание по совпадению таймера 
+    sei(); // включить глобальные прерывания
+
     Serial.println("-------------------------------------------");
     /*Blynk.begin(auth);
     Ethernet.init(53);
@@ -85,12 +105,15 @@ void setup(){
 }
 
 void loop(){
+    static uint8_t sensorCounter = 0;
+
     const uint32_t t = millis();
     static uint32_t prev_t = t;
     static bool prev_but = true;
     const bool but = digitalRead(PIN_BUTTON);
 
     if (t - prev_t > 1000) {
+        Serial.println(seconds);
         digitalWrite(PIN_BUZZER, !but);
         prev_t = t;
     }
@@ -101,8 +124,7 @@ void loop(){
         Serial.println(but);
         
     }
-    for (int i = 0; i < LEAK_CHANELS; i++){
-        CLeakSensor::EState state = leakSensor[i].getState();
-        if (leakSensor[i].getChange()) leakSensor[i].print();
-    }
+    CLeakSensor::EState state = leakSensor[sensorCounter].getState();
+    if (leakSensor[sensorCounter].getChange()) leakSensor[sensorCounter].print();
+    if (++sensorCounter == LEAK_CHANELS) sensorCounter = 0;
 }
