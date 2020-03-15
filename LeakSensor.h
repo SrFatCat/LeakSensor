c6d2622039a952b8f2d29a03ff26607087fca5e7
Sr. Fat Cat <matroskin-2004@mail.ru>
2020-03-15 18:00:07 +0300

    Версия 1.0 готово

--- LeakSensor.h
+++ LeakSensor.h
@@ -1,21 +1,23 @@
 /***********************************************
+Name:		LeakSensor.h
+ Created:	12.03.2020
+ Modified:  15.03.2020
+ Author:	Alex aka Sr.FatCat
 
-
-
-
-
+Classes for LeakSensor sketch
 ***********************************************/
 
 #pragma once
 
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
 class CLeakSensor {
 public:    
     enum EState {STATE_OK, STATE_ERR, STATE_ALARM, STATE_START_ERR};
-
+    enum EstateTrend {TREND_UNDEFINED, TREND_FROM_ERR_TO_OK, TREND_FROM_ERR_TO_ALARM, TREND_FROM_ALARM_TO_ERR}; 
 private:
-    const uint16_t okAverageDiv = 50;
     const uint16_t sampleSize = 1000;
-    
+
     const uint8_t pin;
     int16_t average;
     uint32_t sample = 0;
@@ -23,6 +25,7 @@ private:
     
     CLeakSensor::EState state;
     bool isChange = false;
+    CLeakSensor::EstateTrend stateTrend = TREND_UNDEFINED;
 
     virtual uint16_t valueRead() {return analogRead(pin);}
     int16_t filter(float val);
@@ -32,8 +35,103 @@ public:
     CLeakSensor::EState begin();
     void print() {Serial.print("--- Pin A"); Serial.print(pin-54); Serial.print(" avr "); Serial.print(average); Serial.print(" state "); Serial.println(state);}
     CLeakSensor::EState getState();
-    bool getChange(){return isChange;}
+    inline CLeakSensor::EState State() const {return state;}
+    inline bool getChange() const {return isChange;}
+    inline CLeakSensor::EstateTrend getStatusTrend() const { return stateTrend; }
+    
+    friend class CLeakSensorADS;
 };
+
+class CLeakSensorADS : public CLeakSensor {
+    static bool isInit;
+    static Adafruit_ADS1115 &ads;
+    uint16_t valueRead() final {return ads.readADC_SingleEnded(pin) / 32;}
+public: 
+    CLeakSensorADS(const uint16_t pin_) :  CLeakSensor(pin_){}
+    void init() final { if (!isInit) {ads.setGain(GAIN_ONE); ads.begin(); isInit = true;} }
+};
+
+bool CLeakSensorADS::isInit = false;
+Adafruit_ADS1115 & CLeakSensorADS::ads = *new Adafruit_ADS1115;
+class CBeeper {
+    const uint8_t pin;
+
+    const int8_t alarmBeeperDelay = 6;
+    const int8_t errorBeeperOnDelay = 1;
+    const int8_t errorBeeperOffDelay = 36;
+
+    uint8_t alarmCounter = 0;
+    uint8_t errorCounter = 0;
+    uint8_t tickCounter = 0;
+    bool isRun = false;
+    bool isStop = false;
+public:
+    CBeeper(const uint8_t pin_) : pin(pin_) {};
+    inline void init() { pinMode(pin, OUTPUT); }
+    inline void beep() { tone(pin, 100, 50); } // немедленый бип
+    inline void stop() { isStop = true; }
+    void addEvent(CLeakSensor& );
+    void tick();
+};
+
+void CBeeper::addEvent(CLeakSensor& thisLeakSensor){
+    isStop = false;
+    if (thisLeakSensor.State() == CLeakSensor::EState::STATE_ALARM) {
+        alarmCounter++;
+        if (thisLeakSensor.getStatusTrend() == CLeakSensor::EstateTrend::TREND_FROM_ERR_TO_ALARM) errorCounter--;
+        if (!isRun) digitalWrite(pin, isRun = true);
+        else if (alarmCounter == 1) { // если пищали про ошибку, а тут подвалила протечка
+            tickCounter = 0;
+            digitalWrite(pin, LOW);
+        }
+    }
+    else if (thisLeakSensor.State() == CLeakSensor::EState::STATE_ERR) {
+        errorCounter++;
+        if (thisLeakSensor.getStatusTrend() == CLeakSensor::EstateTrend::TREND_FROM_ALARM_TO_ERR) alarmCounter--;
+        if (!isRun && alarmCounter == 0) digitalWrite(pin, isRun = true);
+    }
+    else if (thisLeakSensor.State() == CLeakSensor::EState::STATE_OK){
+        if (thisLeakSensor.getStatusTrend() == CLeakSensor::EstateTrend::TREND_FROM_ERR_TO_OK) {
+            errorCounter--;
+        }
+        else {
+            alarmCounter--;
+        }
+        if (errorCounter + alarmCounter == 0) digitalWrite(pin, isRun = false);
+    }    
+
+}
+
+void CBeeper::tick(){
+    if ( !isRun ){
+        tickCounter = 0;
+        return;
+    }
+    if (isStop) {
+        digitalWrite(pin, isRun = isStop = false);
+    }
+    tickCounter++;
+    if (alarmCounter > 0){ // пищим тревогу
+        if (tickCounter == alarmBeeperDelay) {
+            digitalWrite(pin, !digitalRead(pin));
+            tickCounter = 0;
+        }
+    }
+    else { // пищим ошибку
+        if (digitalRead(pin)) {
+           if (tickCounter == errorBeeperOnDelay){
+               digitalWrite(pin, LOW);
+               tickCounter = 0;
+           }
+        }
+        else {
+           if (tickCounter == errorBeeperOffDelay){
+               digitalWrite(pin, HIGH);
+               tickCounter = 0;
+           }
+        }
+    }
+}
 
 CLeakSensor::EState CLeakSensor::getState(){
     sample += valueRead();
@@ -44,17 +142,21 @@ CLeakSensor::EState CLeakSensor::getState(){
     int val = sample / sampleSize;
     sample = 0;
     sampleCounter = 0;
-    if (state != STATE_START_ERR && val < 200 || val > 650 ){
+    if (state == STATE_START_ERR) return state;
+    else if (state != STATE_START_ERR && val < 200 || val > 650 ){
         isChange = (state != STATE_ERR);
+        if (isChange && state == STATE_ALARM) stateTrend = TREND_FROM_ALARM_TO_ERR; else stateTrend = TREND_UNDEFINED;
         return state = STATE_ERR;
     }
-    else if (state == STATE_START_ERR) return state;
     else if (abs(average - val) > 150){
         isChange = (state != STATE_ALARM);
+        if (isChange && state == STATE_ERR) stateTrend = TREND_FROM_ERR_TO_ALARM; else stateTrend = TREND_UNDEFINED;
         return state = STATE_ALARM;         
     }
     else {
         isChange = (state != STATE_OK);
+        if (isChange) 
+            if (state == STATE_ERR) stateTrend = TREND_FROM_ERR_TO_OK; else stateTrend = TREND_UNDEFINED;
         return state = STATE_OK;         
        
     }
--- LeakSensor.ino
+++ LeakSensor.ino
@@ -1,19 +1,23 @@
 /*********************************************************************
-LeakSensor
-by Alex.Y.B.
+ Name:		LeakSensor.ino
+ Created:	13.02.2020
+ Modified:  15.03.2020
+ Author:	Alex aka Sr.FatCat
 
-Created: 13.02.2020 modifyed
+LeakSensorsController up to 20 chanels (16 ADC + 4 extended ADS)
 *********************************************************************/
 #include <SPI.h>
 #include <Ethernet.h>
 #define BLYNK_PRINT Serial
-//#define BLYNK_INFO_CONNECTION "W5500"
+#define BLYNK_INFO_CONNECTION "W5500"
 //#define W5100_CS  53
 #include <BlynkSimpleEthernet.h>
 
 #include <Adafruit_NeoPixel.h>
 
 #include "LeakSensor.h"
+
+#include "UniversalKeyboardDriver.h"
 
 const uint8_t PIN_BUTTON = 43;
 const uint8_t PIN_BUZZER = 39;
@@ -22,15 +26,21 @@ const uint8_t PIN_LED_STRIP = 41;
 const uint8_t LEAK_CHANELS = 16;
 
 const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-const char auth[] = "10e3db2a38084530bac9f105d87f7c42";
+const char auth[] = "CpXaKWnOab8-DRUxwMlyeYhVUyotEiEM";
+bool isBlynking = false;
+WidgetLCD lcdBlynk(V0);
+WidgetLED ledBlynk(V1);
 
 Adafruit_NeoPixel stripLED(LEAK_CHANELS, PIN_LED_STRIP, NEO_GRB);
 
-enum EColors {BLACK = 0, RED = 0xFF0000, GREEN = 0x00A000, WHITE = 0xFFFFFF, YELLOW = 0x5F5000};
+enum EColors {BLACK = 0, RED = 0xFF0000, GREEN = 0x00A000, WHITE = 0xFFFFFF, YELLOW = 0x5F5000, DARK_YELLOW=0x131000};
 
-void beep(uint8_t duration, uint8_t cycles){
+CBeeper beeper(PIN_BUZZER);
 
-}
+CKeyboardDriver keyDriver(1);
+const int16_t keyCodeArray[] = { PIN_BUTTON };
+
+//template <typename T> inline Print & operator << (Print &s, T n) { s.print(n); return s; }
 
 CLeakSensor leakSensor[] = {
     CLeakSensor(A13), 
@@ -48,21 +58,46 @@ CLeakSensor leakSensor[] = {
     CLeakSensor(A3),
     CLeakSensor(A2),
     CLeakSensor(A1),
-    CLeakSensor(A0)
+    CLeakSensor(A0),
+    CLeakSensorADS(0),
+    CLeakSensorADS(1),
+    CLeakSensorADS(2),
+    CLeakSensorADS(3)
     };
 
-volatile int seconds;
-
 ISR(TIMER1_COMPA_vect){
-    seconds++;
+    beeper.tick();
+    keyDriver.listenKeys();
 }
 
-void setup(){
-    pinMode(PIN_BUTTON, INPUT_PULLUP);
-    pinMode(PIN_BUZZER, OUTPUT);
-    
+void(* resetFunc) (void) = 0;
+
+void updateLCD(){
+    if (!isBlynking) return;
+    String sNotReady, sError, sAlarm;
+    for (int j = 0; j < LEAK_CHANELS; j++) {
+        if (leakSensor[j].State() == CLeakSensor::EState::STATE_ERR){
+            sError += String(sError.length() == 0 ? "" : ",") + String(j+1);
+        }
+        else if (leakSensor[j].State() == CLeakSensor::EState::STATE_ALARM){
+            sAlarm += String(sAlarm.length() == 0 ? "" : ",") + String(j+1);
+        }
+        else if (leakSensor[j].State() == CLeakSensor::EState::STATE_START_ERR){
+            sNotReady += String(sNotReady.length() == 0 ? "" : ",") + String(j+1);
+        }
+    }
+    String out = (sAlarm.length() > 0 ? "Протеч." + sAlarm + "! " : "") +  (sError.length() > 0 ? "Авар." + sError + " " : "") + (sNotReady.length() > 0 ? "Откл." + sNotReady : "");
+    lcdBlynk.clear();
+    lcdBlynk.print(0,0, out.c_str());
+}
+
+void setup(){    
     Serial.begin(115200);
     Serial.println("*************** START ************\n\n");
+    
+    keyDriver.addDevice((CKeybdDevice *)new CKeybdDigital((int16_t *)keyCodeArray, 1));
+    
+    beeper.init();
 
     stripLED.begin();
     stripLED.fill(WHITE);
@@ -70,61 +105,108 @@ void setup(){
     delay(1000);
 
     for (int j = 0; j < LEAK_CHANELS; j++) {
-        tone(PIN_BUZZER, 100, 50);
+        beeper.beep();
         if (leakSensor[j].begin() == CLeakSensor::EState::STATE_OK) stripLED.setPixelColor(j, GREEN); else stripLED.setPixelColor(j, YELLOW);
         stripLED.show();
         leakSensor[j].print();
+    }
+
+    for (int i = 0; i < 9; i++){
+        if (i%2 == 0) beeper.beep();
+        for (int j = 0; j < LEAK_CHANELS; j++) {
+            if (stripLED.getPixelColor(j) == YELLOW) stripLED.setPixelColor(j, 0);
+            else if (stripLED.getPixelColor(j) == 0) stripLED.setPixelColor(j, YELLOW);
+        }
+        stripLED.show();
+        delay(600);
     }
 
     // инициализация Timer1
     cli();  // отключить глобальные прерывания
     TCCR1A = 0;   // установить регистры в 0
     TCCR1B = 0;
-
-    OCR1A = 15624; // установка регистра совпадения
-
+    OCR1A = 2000;//15624; // установка регистра совпадения ~0.125c
     TCCR1B |= (1 << WGM12);  // включить CTC режим 
     TCCR1B |= (1 << CS10); // Установить биты на коэффициент деления 1024
     TCCR1B |= (1 << CS12);
-
     TIMSK1 |= (1 << OCIE1A);  // включить прерывание по совпадению таймера 
     sei(); // включить глобальные прерывания
-
-    Serial.println("-------------------------------------------");
-    /*Blynk.begin(auth);
+    
     Ethernet.init(53);
     Ethernet.begin(mac, 10000, 10000);
-    if (Ethernet.hardwareStatus() == EthernetNoHardware){
-        Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) Serial.println("Ethernet shield was not found.");
+    else{
+        Serial.println("Ethernet shield was FOUND!!!!!!!!!!!!!!!!");
+        if (Ethernet.linkStatus() == LinkOFF) Serial.println("Ethernet cable is not connected.");
+        else {
+            Serial.println("Ethernet cable is CONNECTED!!!!!!!!!!!!!!!!!!!!!");
+            Blynk.begin(auth);
+            isBlynking = true;
+            updateLCD();
+        }    
     }
-    else Serial.println("Ethernet shield was FOUND!!!!!!!!!!!!!!!!");
 
-    if (Ethernet.linkStatus() == LinkOFF){
-        Serial.println("Ethernet cable is not connected.");
-    } else Serial.println("Ethernet cable is CONNECTED!!!!!!!!!!!!!!!!!!!!!");*/
+    Serial.println("-------------------------------------------");
 }
 
 void loop(){
     static uint8_t sensorCounter = 0;
-
+    static bool isSendNotify = false;
     const uint32_t t = millis();
     static uint32_t prev_t = t;
-    static bool prev_but = true;
-    const bool but = digitalRead(PIN_BUTTON);
 
-    if (t - prev_t > 1000) {
-        Serial.println(seconds);
-        digitalWrite(PIN_BUZZER, !but);
+
+    if (t - prev_t > 500) {
+        bool isAlarm = false;
+        bool isShow = false;
+        for (int j = 0; j < LEAK_CHANELS; j++) {
+            if (leakSensor[j].State() == CLeakSensor::EState::STATE_OK){
+                if (stripLED.getPixelColor(j) != GREEN){
+                    stripLED.setPixelColor(j, GREEN);   
+                    isShow = true; 
+                } 
+            }
+            else if (leakSensor[j].State() == CLeakSensor::EState::STATE_ERR){
+                if (stripLED.getPixelColor(j) != YELLOW) stripLED.setPixelColor(j, YELLOW); else stripLED.setPixelColor(j, DARK_YELLOW);
+                isShow = true;
+            }
+            else if (leakSensor[j].State() == CLeakSensor::EState::STATE_ALARM){
+                if (stripLED.getPixelColor(j) != RED) stripLED.setPixelColor(j, RED); else stripLED.setPixelColor(j, 0);    
+                isAlarm = isShow = true;
+                if (isBlynking && isSendNotify){
+                    Blynk.notify("ПРОТЕЧКА!!!");
+                    isSendNotify = false;
+                }
+            }
+        }
+        static bool isAlarmLed = false;
+        if (isBlynking && (isAlarm || isAlarmLed)){
+            isAlarmLed = !isAlarmLed;
+            if (isAlarmLed) ledBlynk.on(); else ledBlynk.off();
+        }
+        if (isShow) stripLED.show();      
         prev_t = t;
     }
-    if (but != prev_but){
-        prev_but = but;
-        Serial.print(t);
-        Serial.print(" BUT = ");
-        Serial.println(but);
-        
-    }
-    CLeakSensor::EState state = leakSensor[sensorCounter].getState();
-    if (leakSensor[sensorCounter].getChange()) leakSensor[sensorCounter].print();
+    
+    leakSensor[sensorCounter].getState();
+    if (leakSensor[sensorCounter].getChange()){
+        leakSensor[sensorCounter].print();
+        updateLCD();
+        isSendNotify = true;
+        beeper.addEvent(leakSensor[sensorCounter]);
+    } 
     if (++sensorCounter == LEAK_CHANELS) sensorCounter = 0;
+
+    if (isBlynking) Blynk.run();
+
+    if (keyDriver.isKeyPressed()) {
+        bool longPressKey;
+        keyDriver.getKeyCode(&longPressKey);
+		//Serial << " LPK=" << longPressKey << "\n";
+        if (longPressKey) resetFunc(); else beeper.stop();
+	}
+}
+
+BLYNK_CONNECTED() {
+    updateLCD(); 
 }
